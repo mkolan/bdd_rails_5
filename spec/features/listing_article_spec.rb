b6b22a6951da3ca0329413ab97fd5c8e66bb7966
mkolan <madhukar.rk123@gmail.com>
2019-07-17 12:39:06 -0400

    implement display when articles size is 0

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -7,13 +7,29 @@ RSpec.feature "Listing Articles" do
   end
 
   scenario "A user lists all Articles" do
-  visit "/"
-    expect(page).to have_content(@article1.title)
-    expect(page).to have_content(@article1.body)
-    expect(page).to have_content(@article2.title)
-    expect(page).to have_content(@article2.body)
+    visit "/"
+      expect(page).to have_content(@article1.title)
+      expect(page).to have_content(@article1.body)
+      expect(page).to have_content(@article2.title)
+      expect(page).to have_content(@article2.body)
 
-    expect(page).to have_link(@article1.title)
-    expect(page).to have_link(@article2.title)
-  end
+      expect(page).to have_link(@article1.title)
+      expect(page).to have_link(@article2.title)
+    end
+
+  scenario "A user has no Articles" do
+    Article.delete_all ## This will make sure that the articles created in the before block will gets deleted (test_db)
+    visit "/"
+      expect(page).not_to have_content(@article1.title)
+      expect(page).not_to have_content(@article1.body)
+      expect(page).not_to have_content(@article2.title)
+      expect(page).not_to have_content(@article2.body)
+
+      expect(page).not_to have_link(@article1.title)
+      expect(page).not_to have_link(@article2.title)
+
+      within ("h1#no-articles") do
+        expect(page).to have_content("No articles created")
+      end
+    end
 end
